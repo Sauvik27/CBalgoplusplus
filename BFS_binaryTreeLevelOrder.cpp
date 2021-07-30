5672e9f3cf58fcf9d5a6a126296b3666f293c836
Sauvik Kumar <57688017+Sauvik27@users.noreply.github.com>
2021-07-30 20:35:57 +0530

    Update BFS_binaryTreeLevelOrder.cpp

--- BFS_binaryTreeLevelOrder.cpp
+++ BFS_binaryTreeLevelOrder.cpp
@@ -15,15 +15,6 @@ public:
 	}
 };
 
-int height(node* root){
-	if(root == NULL){
-		return 0;
-	}
-	int ls = height(root->left);
-	int rs = height(root->right);
-	return max(ls,rs) + 1;
-}
-
 node* binaryTree(){
 	int data;
 	cin>>data;
@@ -36,35 +27,6 @@ node* binaryTree(){
 	return root;
 }
 
-void printKthLevel(node* root, int k){
-	if(root == NULL){
-		return;
-	}
-	if(k==1){
-		cout<<root->data<<" ";
-	}
-	printKthLevel(root->left,k-1);
-	printKthLevel(root->right,k-1);
-	return;
-}
-void printAllLevels(node* root, int height){
-	if(root == NULL){
-		return;
-	}
-	for(int i=1;i<=height;i++){
-		cout<<"\n";
-		printKthLevel(root, i);
-	}
-}
-
-void display(node* root){
-	if(root == NULL){
-		return;
-	}
-	cout<<root->data<<" ";
-	display(root->left);
-	display(root->right);
-}
 
 void bfs(node* root){
 	queue<node*> q;
